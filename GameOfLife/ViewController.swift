a695232f58966393c62fe441d9794e031c0e303d
Bipin Gohel <Bipin-Laxman.Gohel@t-systems.com>
2018-05-11 11:11:13 +0200

    Initial Commit

---
+++ GameOfLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Bipin Gohel on 11.05.18.
+//  Copyright © 2018 Bipin Gohel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
